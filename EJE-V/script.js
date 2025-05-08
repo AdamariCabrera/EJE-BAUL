e5cb162ccad0ad1a6fb54761aa2d1527999a1d2e
AdamariCabrera <162341584+AdamariCabrera@users.noreply.github.com>
2025-05-07 18:43:54 -0600

    Delete EJE-V directory

--- EJE-V/script.js
+++
@@ -1,6 +0,0 @@
-const imagenes = document.querySelectorAll('.imagen-boton');
-imagenes.forEach(imagen => {
-    imagen.addEventListener('click', function() {
-        console.log('Imagen ' + imagen.alt + ' presionada');
-    });
-});
